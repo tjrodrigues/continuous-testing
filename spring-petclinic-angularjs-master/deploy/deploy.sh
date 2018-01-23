8ec9741da071e26f64702e3cc436d45d366fa48e
CRITICAL\tjrodrigues <tjrodrigues@criticalsoftware.com>
2018-01-23 00:08:08 +0000

    .

--- spring-petclinic-angularjs-master/deploy/deploy.sh
+++ spring-petclinic-angularjs-master/deploy/deploy.sh
@@ -3,15 +3,12 @@
 # $2 -> application version
 # $3 -> application home directory
 
-
-# sudo mkdir -p $HOME/petclinic
-sudo mkdir -p $3/deploy/
-wget http://$1/repository/maven-releases/org/criticalsoftware/sspa/petclinic/$2/petclinic-$2.tar.gz -O $3/petclinic-$2.tar.gz
-sudo chmod +x $3/petclinic-$2.tar.gz
-sudo tar -xvf $3/petclinic-$2.tar.gz -C $3/deploy/
-cd $3/deploy/spring-petclinic-angularjs-master
+sudo mkdir -p $3/app-deploy
+sudo wget http://$1/repository/maven-releases/org/criticalsoftware/sspa/petclinic/$2/petclinic-$2.tar.gz -O $3/app-deploy/petclinic-$2.tar.gz
+sudo chmod +x $3/app-deploy/petclinic-$2.tar.gz
+sudo tar -xvf $3/app-deploy/petclinic-$2.tar.gz -C $3/app-deploy
+cd $3/app-deploy/spring-petclinic-angularjs-master
 sudo chmod -R +x ./*
-./mvnw clean install
-cd spring-petclinic-server
-../mvnw spring-boot:run 
-
+sudo ./mvnw clean install
+cd $3/app-deploy/spring-petclinic-angularjs-master/spring-petclinic-server
+sudo ../mvnw spring-boot:run &
