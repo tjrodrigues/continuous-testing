eee06d8b35a4624ed1c5bf8e2e364308039c9bf8
CRITICAL\tjrodrigues <tjrodrigues@criticalsoftware.com>
2018-03-12 00:12:20 +0000

    .

--- spring-petclinic-angularjs-master/tests/api/run-soapui-tests.sh
+++ spring-petclinic-angularjs-master/tests/api/run-soapui-tests.sh
@@ -4,6 +4,6 @@ testServer=$2
 soapuiProject=$3
 reportFolder=$4
 
-$soapuiHome\bin\testrunner.sh $soapuiProject -a -A -f$reportFolder -j -J -r -a -GbaseURL=$testServer
+$soapuiHome/bin/testrunner.sh $soapuiProject -a -A -f$reportFolder -j -J -r -a -GbaseURL=$testServer
 
 
